3305b66d8a5174f395bd49fd530085f88408932b
yqkheart <32261107+yqkheart@users.noreply.github.com>
2017-12-15 08:52:46 +0800

    Update rem.js

--- boke/js/rem.js
+++ boke/js/rem.js
@@ -14,7 +14,8 @@
 
         if (!doc.addEventListener) return;
         win.addEventListener(resizeEvt, recalc, false);
-        doc.addEventListener('DOMContentLoaded', recalc, false);
+//         doc.addEventListener('DOMContentLoaded', recalc, false);
+        recalc();
     })(document, window);
 
 
